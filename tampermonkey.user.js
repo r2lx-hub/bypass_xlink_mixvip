a93dc3859933f4625065d0ea7ab8bfde084597fc
r2lx-hub <r2lxhub@gmail.com>
2025-07-18 17:56:36 +0700

    Add files via upload

---
+++ tampermonkey.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name         Bypass xLink_mixvip (Tool)
+// @namespace    http://tampermonkey.net/
+// @version      Beta
+// @description  Auto vượt xLink_mixvip không cần chạm tay
+// @author       By R2lx
+// @require      https://raw.githubusercontent.com/r2lx-hub/bypass_xlink_mixvip/refs/heads/main/bypass_xlink_mixvip.js
+// @match        https://linkday.xyz/*
+// @grant        none
+// @icon         
+// ==/UserScript==
